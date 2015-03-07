f2d317f308c5984d9da0cc1f4556751fa39c4ca6
Nalitha Liyanage <nalitha770@gmail.com>
2015-03-07 10:21:52 -0800

    video 38

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -1,4 +1,4 @@
-//class that runs all the timers and things that aren't inside any of the other entities
+//class that runs all the timers and occurences that aren't inside any of the other entities
 game.GameTimerManager = Object.extend({
     //constructor function
     init: function(x, y, settings){
@@ -6,7 +6,8 @@ game.GameTimerManager = Object.extend({
         this.now = new Date().getTime();
         //keeps track of last time creep was made
         this.lastCreep = new Date().getTime();
-        this.paused = false;
+        //says the game is not paused
+        this.paused = game.data.paused;
         //keeps the function updating
         this.alwaysUpdate = true;
     },
@@ -14,7 +15,9 @@ game.GameTimerManager = Object.extend({
     update: function(){
         //keeps track of timer
         this.now = new Date().getTime();
+        
         this.goldTimerCheck();
+        this.creepTimerCheck();
 
         //updates
         return true;
@@ -27,33 +30,38 @@ game.GameTimerManager = Object.extend({
             console.log("Current gold: " + game.data.gold);
         }
     },
+
     creepTimerCheck: function(){
         //checks to make sure there is a multiple of ten. makes sure its been at least a second since last creep has been made
         if(Math.round(this.now/1000)%10 === 0 && (this.now - this.lastCreep >= 1000)){
             //updates timer
             this.lastCreep = this.now;
-            //creates and inserts creep into worls
+            //creates and inserts creeps into world
             var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-            //adds the creeps to the worls
+            //adds the creeps to the world
             me.game.world.addChild(creepe, 5);
-        }
+        } 
     }
 });
-    
+
+
 game.HeroDeathManager = Object.extend({
     init: function(x, y, settings){
-        //keeps the function updating
         this.alwaysUpdate = true;
     },
 
     update: function(){
+        //runs if player is dead
         if(game.data.player.dead){
+            //takes the player off the screen
             me.game.world.removeChild(game.data.player);
+            //runs the resetPlayer function
             me.state.current().resetPlayer(10, 0);
-        }
-    return true;
 
-    },  
+        }
+
+        return true;
+    }
 });
 
 game.ExperienceManager = Object.extend({
@@ -61,17 +69,27 @@ game.ExperienceManager = Object.extend({
         this.alwaysUpdate = true;
         this.gameOver = false;
     },
-
     update: function(){
-        if(game.data.win === true && !this.gameOver){
-            game.data.exp += 10;
-            this.gameOver = true;
-        }else if(game.data.win === false && !this.gameOver){
-            game.data.exp += 1;
-            this.gameOver = true;
+        if (game.data.win === true && !this.gameOver) {
+            this.gameOver(true);
         }
-        console.log(game.data.exp);
+        else if (game.data.win === false && !this.gameOver) {
+            this.gameOver(false);
+        }
 
+        console.log(game.data.exp);
         return true;
     },
+
+    gameOver: function(win){
+        if(win){
+            game.data.exp += 10;
+        }
+        else{
+            game.data.exp += 1;
+        }
+        
+        this.gameOver = true;
+        me.save.exp = game.data.exp;
+    }
 });
--- js/screens/title.js
+++ js/screens/title.js
@@ -3,34 +3,24 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		//inserts the title-screen image into the map
+		//calls the image you set as the title screen
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-		//makes the enter key into an object
-		me.input.bindKey(me.input.KEY.ENTER, "start");
 
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
-				//calls super class 
-				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
-				//sets the font to arial, the size 46, and colors it white
-				this.font = new me.Font("GEUVARA_.TTF", 46, "white");
+				//renders the text
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+				//font and font color
+				this.font = new me.Font("Arial", 46, "white");
 			},
-			//function that sets up the writing
+
 			draw: function(renderer){
-				//inserts the message "Awesomenauts!" and sets where writing starts
-				this.font.draw(renderer.getContext(), "Awesomenauts!", 42, 60);
-				//inserts the message "Press ENTER to play!" and sets where writing starts
-				this.font.draw(renderer.getContext(), "ENTER to play!", 700, 60);
+				//sets the position on the screen for the text
+				this.font.draw(renderer.getContext(), "START A NEW GAME", 450, 130);
+				this.font.draw(renderer.getContext(), "Press ENTER to play", this.pos.x, this.pos.y);
+
 			}
 		})));
-		//listens to see if enter button is pressed
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-			//runs if enter button is pressed
-			if (action === "start") {
-				//goes to play screen
-				me.state.change(me.state.PLAY);
-			}
-		});
 	},
 	
 	
@@ -38,9 +28,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		//removes the enter key's status as an object
-		me.input.unbindKey(me.input.KEY.ENTER); 
-		//inform's program not to listen for pressed enter key
-		me.event.unsubscribe(this.handler);
+
 	}
 });
